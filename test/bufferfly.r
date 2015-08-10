7e598b03ac117492cff5c1b1526f6f1057884871
foreverbell <dql.foreverbell@gmail.com>
2015-08-10 15:19:19 +0800

    lrc fix

--- test/bufferfly.r
+++ test/bufferfly.r
@@ -9,27 +9,27 @@ Dakedo wow wow～ ashita no yotei mo wakaranai
 Mugendai na yume no ato no nanimo nai yo no naka ja 
 Sou sa itoshii omoi mo makesou ni naru kedo 
 Stay shigachi na imeeji darake no tayorinai tsubasa demo 
-Kitto toberu sa on my love 
+Kitto toberu sa On my love 
 
 Ukareta chou ni natte ichizu na kaze ni notte 
 Doko mademo kimi ni ai ni yukou 
 Aimai na kotoba tte igai ni benri datte 
 Sakenderu hitto songu kikinagara 
 
-Nani ga wow wow wow wow wow kono machi ni hibiku no darou 
-Dakedo wow wow wow wow wow kitai shitetemo shikata nai 
+Nani ga wow wow～ kono machi ni hibiku no darou 
+Dakedo wow wow～ kitai shitetemo shikata nai 
 
 Mugendai na yume no ato no yarusenai yo no naka ja 
 Sou sa joushiki hazure mo waruku wa nai ka na 
 Stay shisou na imeeji wo someta gikochinai tsubasa demo 
-Kitto toberu sa on my love 
+Kitto toberu sa On my love 
 
 Mugendai na yume no ato no nanimo nai yo no naka ja 
 Sou sa itoshii omoi mo makesou ni naru kedo 
 Stay shigachi na imeeji darake no tayorinai tsubasa demo 
-Kitto toberu sa oh yeah 
+Kitto toberu sa Oh yeah 
 
 Mugendai na yume no ato no yarusenai yo no naka ja 
 Sou sa joushiki hazure mo waruku wa nai ka na 
 Stay shisou na imeeji wo someta gikochinai tsubasa demo 
-Kitto toberu sa on my love
+Kitto toberu sa On my love
