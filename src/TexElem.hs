5c87e13660d24f84cdab9b3951a9ca5ffbe5d1ee
foreverbell <dql.foreverbell@gmail.com>
2015-08-13 10:56:22 +0800

    separator between romajis

--- src/TexElem.hs
+++ src/TexElem.hs
@@ -23,12 +23,15 @@ data TexElem = Line
              | Katakana String String      -- katakana, romaji
              deriving (Show)
 
+-- map over list but head element
+map' f (x:xs) = x : map f xs
+
 texify :: TexElem -> Text
 texify doc = case doc of
   Line         -> T.pack $ " \\\\ \n"
   Break        -> T.pack $ "\\, "
   Lit s        -> T.pack $ (build mainFont s) ++ " "
-  Kanji k h r  -> T.pack $ printf "\\ruby{%s%s}{%s} " (build mainFont k) (build rubyFont ("(" ++ (concat h) ++ ")")) (build romajiFont (concat r))
+  Kanji k h r  -> T.pack $ printf "\\ruby{%s%s}{%s} " (build mainFont k) (build rubyFont ("(" ++ (concat h) ++ ")")) (build romajiFont (concat (map' ((:) '-') r)))
   Hiragana h r -> T.pack $ printf "\\ruby{%s}{%s} " (build mainFont h) (build romajiFont r)
   Katakana k r -> T.pack $ printf "\\ruby{%s}{%s} " (build mainFont k) (build romajiFont r)
   where 
