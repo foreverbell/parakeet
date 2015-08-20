a08744350d02e954501acbe6d1f175d737fe5468
foreverbell <dql.foreverbell@gmail.com>
2015-08-20 11:41:41 +0800

    runParser with file path

--- src/Parser/Parser.hs
+++ src/Parser/Parser.hs
@@ -8,6 +8,7 @@ import           Data.Char (toLower)
 import           Parser.Stage0 (stage0)
 import           Parser.Stage1 (stage1)
 
+import           Options (Options(..))
 import qualified Element as E
 
 -- import System.IO.Unsafe
@@ -19,14 +20,14 @@ setLine l = do
   pos <- getPosition
   setPosition $ setSourceLine pos l
 
-parseLine :: Line -> String -> String -> Either ParseError [E.Element]
-parseLine l j r = do
-  wds <- runParser (setLine l >> stage0) () "Japanese" j 
-  runParser (setLine l >> stage1) wds "Romaji" (lowerCase r)
+parseLine :: Options -> Line -> String -> String -> Either ParseError [E.Element]
+parseLine opts l j r = do
+  wds <- runParser (setLine l >> stage0) () (optJInputFile opts) j 
+  runParser (setLine l >> stage1) wds (optRInputFile opts) (lowerCase r)
   -- where evil = (unsafePerformIO . putStrLn . concatMap (\token -> (T.unwrapToken token) ++ " ")) wds
 
-doParse :: String -> String -> [E.Element]
-doParse j r = fromEither $ fmap concat $ sequence $ zipWith3 parseLine [1 .. ] (lines j) (lines r) 
+doParse :: Options -> String -> String -> [E.Element]
+doParse opts j r = fromEither $ fmap concat $ sequence $ zipWith3 (parseLine opts) [1 .. ] (lines j) (lines r) 
   where
     fromEither (Left err) = error $ show err
     fromEither (Right va) = va
