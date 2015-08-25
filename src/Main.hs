2b0c0d07f3b76151068a1ab2a496783fc2497c47
foreverbell <dql.foreverbell@gmail.com>
2015-08-25 16:56:15 +0800

    add .ghci

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,6 @@
 module Main (main) where
 
-import           System.Environment (getArgs, withArgs)
+import           System.Environment (getArgs)
 
 import           Options (Options(..), runOpts)
 import           Print (prettyPrint)
@@ -11,9 +11,3 @@ main = do
   case prettyPrint opts of 
     Left err -> fail err
     Right r  -> put r
-
-test = do
-  withArgs (buildArgs "../tests/Anonymous/Anonymous.j" "../tests/Anonymous/Anonymous.r" "Anonymous.tex") main
-  withArgs (buildArgs "../tests/Butter-fly/Butter-fly.j" "../tests/Butter-fly/Butter-fly.r" "Butter-fly.tex") main
-  where buildArgs j r t = ["-j", j, "-r", r, "-o", t]
-
