00b6cefb40e4304d1c3522ac6d932b7df0b9cb1a
foreverbell <dql.foreverbell@gmail.com>
2015-09-02 14:14:55 +0800

    No evil modification xD

--- tests/Butter-fly/Butter-fly.r
+++ tests/Butter-fly/Butter-fly.r
@@ -3,8 +3,8 @@ Ima sugu kimi ni ai ni yukou
 Yokei na koto nante wasureta hou ga mashi sa 
 Kore ijou shareteru jikan wa nai 
 
-Nani ga wow wow～ kono sora ni todoku no darou 
-Dakedo wow wow～ ashita no yotei mo wakaranai 
+Nani ga wow wow~ kono sora ni todoku no darou 
+Dakedo wow wow~ ashita no yotei mo wakaranai 
 
 Mugendai na yume no ato no nanimo nai yo no naka ja 
 Sou sa itoshii omoi mo makesou ni naru kedo 
@@ -16,8 +16,8 @@ Doko mademo kimi ni ai ni yukou
 Aimai na kotoba tte igai ni benri datte 
 Sakenderu hitto songu kikinagara 
 
-Nani ga wow wow～ kono machi ni hibiku no darou 
-Dakedo wow wow～ kitai shitetemo shikata nai 
+Nani ga wow wow~ kono machi ni hibiku no darou 
+Dakedo wow wow~ kitai shitetemo shikata nai 
 
 Mugendai na yume no ato no yarusenai yo no naka ja 
 Sou sa joushiki hazure mo waruku nai ka na 
